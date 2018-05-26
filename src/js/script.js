c6226efa0ca3743f3b3b02c7eaebd39a473ffb2d
Christopher Lampert <christopherlampert@rentalcars.com>
2018-05-26 22:29:47 +0100

    added clock configuration to a function

--- src/js/script.js
+++ src/js/script.js
@@ -15,75 +15,80 @@ var copyright = document.createElement('p');
 copyright.innerHTML = 'Copyright &copy; ' + year + ' | Christopher Lampert';
 footerContentWrap.appendChild(copyright);
 
-switch (day) {
-    case 1:
-        day = "Mon";
-        break;
-    case 2:
-        day = "Tue";
-        break;
-    case 3:
-        day = "Wed";
-        break;
-    case 4:
-        day = "Thurs";
-        break;
-    case 5:
-        day = "Fri";
-        break;
-    case 6:
-        day = "Sat";
-        break;
-    case 7:
-        day = "Sun";
-        break;
-    default:
-        day = "Error: Unknown Day";
-        break;
+function configureClock() {
+    switch (day) {
+        case 1:
+            day = "Mon";
+            break;
+        case 2:
+            day = "Tue";
+            break;
+        case 3:
+            day = "Wed";
+            break;
+        case 4:
+            day = "Thurs";
+            break;
+        case 5:
+            day = "Fri";
+            break;
+        case 6:
+            day = "Sat";
+            break;
+        case 7:
+            day = "Sun";
+            break;
+        default:
+            day = "Error: Unknown Day";
+            break;
+    }
+
+    switch (month) {
+        case 0:
+            month = "Jan";
+            break;
+        case 1:
+            month = "Feb";
+            break;
+        case 2:
+            month = "Mar";
+            break;
+        case 3:
+            month = "Apr";
+            break;
+        case 4:
+            month = "May";
+            break;
+        case 5:
+            day = "Jun";
+            break;
+        case 6:
+            month = "Jul";
+            break;
+        case 7:
+            month = "Aug";
+            break;
+        case 8:
+            month = "Sep";
+            break;
+        case 9:
+            month = "Oct";
+            break;
+        case 10:
+            month = "Nov";
+            break;
+        case 11:
+            month = "Dec";
+            break;
+        default:
+            month = "Error: Unknown Month";
+            break;
+    }
+    dateField.innerHTML = "<p>" + day + " " + month + " " + date + " " + year + "</p>";
 }
 
 
-switch (month) {
-    case 0:
-        month = "Jan";
-        break;
-    case 1:
-        month = "Feb";
-        break;
-    case 2:
-        month = "Mar";
-        break;
-    case 3:
-        month = "Apr";
-        break;
-    case 4:
-        month = "May";
-        break;
-    case 5:
-        day = "Jun";
-        break;
-    case 6:
-        month = "Jul";
-        break;
-    case 7:
-        month = "Aug";
-        break;
-    case 8:
-        month = "Sep";
-        break;
-    case 9:
-        month = "Oct";
-        break;
-    case 10:
-        month = "Nov";
-        break;
-    case 11:
-        month = "Dec";
-        break;
-    default:
-        month = "Error: Unknown Month";
-        break;
-}
+
 
 function checkTimes() {
     if (sec < 10 && typeof sec === "number") {
@@ -127,7 +132,7 @@ function runTheClock() {
     }
 }
 
-dateField.innerHTML = "<p>" + day + " " + month + " " + date + " " + year + "</p>";
+configureClock();
 updateClock();
 runTheClock();
 setInterval(runTheClock, 1000);
