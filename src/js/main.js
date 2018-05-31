284902bc6967fea786a6ab5e8e0bc60ff140fac2
Christopher Lampert <christopherlampert@rentalcars.com>
2018-05-31 21:52:02 +0100

    refactored menu so it now opens and closes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,43 @@
 'use strict';
 
+var openMenuButton = document.getElementById('open-menu');
+var closeMenuButton = document.getElementById('close-menu');
+var navMenu = document.getElementById('side-menu');
+var content = document.getElementsByClassName('content-wrap');
+var links = document.getElementsByClassName('nav-link');
+
+function toggleInert(elementList) {
+    for (var i=0; i < elementList.length; i++) {
+        if (elementList[i].hasAttribute('inert')) {
+            elementList[i].removeAttribute('inert');
+        } else {
+            elementList[i].setAttribute('inert', '');
+        }
+    }
+}
+
+for(var i = 0 ; i < links.length; i++){
+    links[i].addEventListener("click", function(){
+        navMenu.classList.remove('side-menu--open');
+        navMenu.classList.add('side-menu--closed');
+        toggleInert(content);
+    });
+}
+
+openMenuButton.addEventListener("click", function(){
+    navMenu.classList.remove('side-menu--closed');
+    navMenu.classList.add('side-menu--open');
+    toggleInert(content);
+    closeMenuButton.focus();
+});
+
+closeMenuButton.addEventListener("click", function () {
+   navMenu.classList.remove('side-menu--open');
+   navMenu.classList.add('side-menu--closed');
+    toggleInert(content);
+   openMenuButton.focus();
+});
+
 createFooter();
 configureClock();
 updateClock();
